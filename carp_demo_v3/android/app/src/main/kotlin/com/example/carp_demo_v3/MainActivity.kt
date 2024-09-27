98bbe6557249e4371f026a0691d59e1585bc1618
KamathN593 <nk2017@kamaths.com>
2024-09-27 18:40:05 -0400

    first commit

---
+++ carp_demo_v3/android/app/src/main/kotlin/com/example/carp_demo_v3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.carp_demo_v3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ carp_demo_v3/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:carp_core/carp_core.dart';
+import 'package:carp_mobile_sensing/carp_mobile_sensing.dart';
+import 'package:carp_survey_package/survey.dart';
+
+/// This is a very simple example of how this sampling package is used with
+/// CARP Mobile Sensing (CAMS).
+/// NOTE, however, that the code below will not run.
+/// See the documentation on how to use CAMS: https://github.com/cph-cachet/carp.sensing-flutter/wiki
+void main() async {
+  // register this sampling package before using its measures
+  SamplingPackageRegistry().register(SurveySamplingPackage());
+
+  // Create a study protocol
+  StudyProtocol protocol = StudyProtocol(
+    ownerId: 'owner@dtu.dk',
+    name: 'Survey Example',
+  );
+
+  // define which devices are used for data collection
+  // in this case, its only this smartphone
+  Smartphone phone = Smartphone();
+  protocol.addMasterDevice(phone);
+
+  // add a WHO-5 survey as an app task
+  // plus collect device and ambient light information when survey is done
+  protocol.addTriggeredTask(
+      DelayedTrigger(delay: Duration(seconds: 30)),
+      RPAppTask(
+          type: SurveyUserTask.SURVEY_TYPE,
+          name: 'WHO-5 Survey',
+          rpTask: who5Task)
+        ..measures.add(Measure(type: DeviceSamplingPackage.DEVICE))
+        ..measures.add(Measure(type: SensorSamplingPackage.LIGHT)),
+      phone);
+}
